27be8f7c16cab7e7b214f894defd663b95af085e
EsterkinPetr <petruhaest@gmail.com>
2023-01-27 00:37:31 +0300

    Д/З_4

--- Task_4.2.py
+++ Task_4.2.py
@@ -1,12 +1,20 @@
-vvod = input().split()
-if vvod[1] == '+':
-    print(float(vvod[0]) + float(vvod[2]))
-elif vvod[1] == '-':
-    print(float(vvod[0]) - float(vvod[2]))
-elif vvod[1] == '*':
-    print(float(vvod[0]) * float(vvod[2]))
-elif vvod[1] == '/':
-    if float(vvod[2]) == 0:
-        print('Делить на ноль нельзя!')
-    else:
-        print(float(vvod[0]) / float(vvod[2]))
+pr1 = input()
+pr2 = input()
+pr1 = pr1.lower()
+pr2 = pr2.lower()
+dict1 = {}
+dict2 = {}
+for i in pr1:
+    if (str(i)).isalpha():
+        if i not in dict1:
+            dict1[i] = 0
+        dict1[i] += 1
+for i in pr2:
+    if (str(i)).isalpha():
+        if i not in dict2:
+            dict2[i] = 0
+        dict2[i] += 1
+if dict1 == dict2:
+    print('True')
+else:
+    print('False')
