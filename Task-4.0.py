0e3db32dd7895883198c02e10ac5b6ae2bc52c68
EsterkinPetr <petruhaest@gmail.com>
2023-01-26 21:08:25 +0300

    Д/З_4

---
+++ Task-4.0.py
@@ -0,0 +1,15 @@
+s, z = input().split()
+sls = {}
+slz = {}
+for i in s:
+    if i not in sls:
+        sls[i] = 0
+    sls[i
+for j in z:
+    if j not in slz:
+        slz [j] = 0
+    slz[j] += 1
+if sls == slz:
+     print('True')
+else:
+    print('False')
