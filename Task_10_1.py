47badf34b3510fc879fda41547ad847e57b18dc0
EsterkinPetr <petruhaest@gmail.com>
2023-02-16 18:10:46 +0300

    Task_10_1-Исправленная

--- Task_10_1.py
+++ Task_10_1.py
@@ -1,12 +1,12 @@
 import openpyxl
-from openpyxl import Workbook
+#from openpyxl import Workbook
 #wb = Workbook()
 #wb.save('Task_10-1.xlsx')
 wb = openpyxl.load_workbook('Task_10-1.xlsx')
 ws = wb['Sheet']
 ws2 = wb['Итоги']
 sotrsum = {}
-for r in range(2, ws.max_row):
+for r in range(1, ws.max_row + 1):
     sotr = ws.cell(row=r, column=1).value
     sotrsum[sotr] = sotrsum.get(sotr, 0) + ws.cell(row=r, column=2).value
 ws2.cell(row=1, column=1).value = 'Фамилия'
