5cc97fbf0bd855432d9ed4e37d8aaed3bdb35842
EsterkinPetr <petruhaest@gmail.com>
2023-02-18 13:11:39 +0300

    Д/з_13.

--- Task_10_2.py
+++ Task_10_2.py
@@ -1,5 +1,5 @@
 import openpyxl
-#rom openpyxl import Workbook
+#from openpyxl import Workbook
 #wb = Workbook()
 
 wb = openpyxl.load_workbook('Task_10-2.xlsx')
@@ -14,20 +14,21 @@ for r in range(1, ws1.max_row + 1):
 for r in range(1, ws2.max_row + 1):
     f2 = ws2.cell(row=r, column=1).value
     fs2[f2] = fs1.get(f2, 0) + ws2.cell(row=r, column=2).value
-print(fs1, fs2)
+#print(fs1, fs2)
 fsi = {}
 fsi.update(fs2)
+#print(fsi)
 for f in fs1.keys():
     fsi[f] = fsi.get(f, 0) + fs1[f]
 sfsi = dict(sorted(fsi.items(), key=lambda x: x[0]))
-ws3.cell(row=1, column=1).value = 'Фамилия'
-ws3.cell(row=1, column=2).value = 'Сумм. выработка'
+#ws3.cell(row=1, column=1).value = 'Фамилия'
+#ws3.cell(row=1, column=2).value = 'Сумм. выработка'
 for k, v in sfsi.items():
     ws3.append([k, v])
 itogo = 0
-for i in range(2, ws3.max_row + 1):
+for i in range(1, ws3.max_row + 1):
     itogo += int(ws3.cell(row=i, column=2).value)
 ws3.append(['ИТОГО', str(itogo)])
-print(fsi)
-print(sfsi)
+#print(fsi)
+#print(sfsi)
 wb.save('Task_10-2.xlsx')
