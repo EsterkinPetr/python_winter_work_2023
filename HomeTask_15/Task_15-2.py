d00e9dd4e5c8b1ded7826bf934a4bc4dd3cc7689
EsterkinPetr <petruhaest@gmail.com>
2023-02-23 10:12:28 +0300

    Д/з_15

--- HomeTask_15/Task_15-2.py
+++ HomeTask_15/Task_15-2.py
@@ -1,6 +1,6 @@
 import re
 def grz(ssim):
-    res = re.findall(r'\b[ABEKMHOPCTYX]\d\d\d[ABEKMHOPCTYX]{2}1?78\b', ssim)
+    res = re.findall(r'[ABEKMHOPCTYX]\d\d\d[ABEKMHOPCTYX]{2}1?78', ssim)
     print(*res)
     return
-grz('A672CB78 A123BC78 X666XX178 A001AA78 B777BW178 JK555CC178')
+grz('dgvA672CB78A123BC78X666XnbchbcX178 A001AA278 B777BW178 JK555CC17812nb78h')
