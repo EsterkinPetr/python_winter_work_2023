de8553b8dcc74a20e91e9310e0929726f245c875
EsterkinPetr <petruhaest@gmail.com>
2023-03-02 19:41:05 +0300

    Д/з_17

--- HomeTask_17/Task_17-2.py
+++ HomeTask_17/Task_17-2.py
@@ -1,18 +1,36 @@
 def deco(fu):
     def wraper(*args, **kwargs):
         lta = []
+        ta = ''
         for x in args:
-            xstr = str(x)
-            for el in xstr:
-                if el.isalpha():
-                    lta.append(el.upper())
+            if type(x) == str:
+                for el in x:
+                     if el.isalpha():
+                         el = el.upper()
+                         ta += el
+                     else:
+                         ta += el
+                lta.append(ta)
+                ta = ''
+        for x in kwargs:
+            if type(kwargs[x]) == str:
+                print(kwargs[x])
+                for el in (kwargs[x]):
+                    if el.isalpha():
+                        el = el.upper()
+                        ta += el
+                    else:
+                        ta += el
+                lta.append(ta)
+                ta = ''
         value = fu(*args, **kwargs)
         return lta
     return wraper
 @deco
 def fu(*args, **kwargs):
     return "Готово!"
-print(fu('asdfg', 5, a = 2))
+print(fu('asd5fg','zxc',  2, 5, a = 2,b = 'zyu3gh', c = '2jklb'))
+
 
 
 
