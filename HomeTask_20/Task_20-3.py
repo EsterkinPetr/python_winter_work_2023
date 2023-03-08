19319a237d649b69cfb49bcf280407808bce5971
EsterkinPetr <petruhaest@gmail.com>
2023-03-08 23:44:18 +0300

    Д/з_20

---
+++ HomeTask_20/Task_20-3.py
@@ -0,0 +1,29 @@
+class Gener:
+
+    def __init__(self):
+        self.x = -1
+        self.s = []
+        self.let = []
+        for i in range(ord('A'), ord('Z') + 1):
+            self.let.append(chr(i))
+        for i in range(1, 27):
+            self.s.append(i)
+            self.s.append(self.let[i - 1])
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        y = self.s[self.x - len(self.s) + 1]
+        self.x += 1
+        return y
+
+
+a = Gener()
+for i in range(104):
+    print(next(a), end=',')
+
+
+
+
+
