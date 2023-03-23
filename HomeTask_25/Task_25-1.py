7743c7bb667c7be5ccac16c75cb23548607fafa9
EsterkinPetr <petruhaest@gmail.com>
2023-03-23 23:41:18 +0300

    Д/з_25

---
+++ HomeTask_25/Task_25-1.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import (QWidget, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QApplication)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('My_app')
+        layout = QHBoxLayout()
+        button1 = QPushButton('Кнопка 1')
+        button2 = QPushButton('Кнопка2')
+        layout.addWidget(button1)
+        layout.addWidget(button2)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
+
